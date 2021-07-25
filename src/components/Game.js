41bb974f0051d9daef65f06f807e2996bf72abef
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-07-25 19:52:21 +0530

    Display 1D array on board cells

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,16 @@
 import Board from "./Board";
 
-import './styles/Game.scss'
+import "./styles/Game.scss";
 
 const Game = () => {
-
-return(
-    <div className={'gamePage'}>
-        <Board/>
+  const s =
+    "000051030005000040074002000496070300700208900208009475001007890827590160640010500";
+  const gameMatrix = s.split("").map((x) => parseInt(x));
+  return (
+    <div className={"gamePage"}>
+      <Board gameMatrix={gameMatrix} />
     </div>
-)
-}
+  );
+};
 
 export default Game;
