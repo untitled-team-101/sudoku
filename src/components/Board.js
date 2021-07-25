b4c71e4fa941c496b59ede10bc55666947b696e5
grawish <grawish06@gmail.com>
2021-07-25 12:08:36 +0530

    3d box

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,33 @@
 import './styles/Board.scss'
+
 const Board = () => {
 
     let arr = []
-    for(let i=0;i<81;i++){
+    for (let i = 0; i < 81; i++) {
         arr.push(<div className={'cell'}>1</div>)
     }
 
-    return(
+    return (
         <div className={'boardContainer'}>
-            <div className={'board'}>
-                {
-                    arr
-                }
+            <div  className={'board'}>
+                <div className="page top">
+
+                </div>
+                <div className="page bottom">
+
+                </div>
+                <div className="page left">
+
+                </div>
+                <div className="page right">
+
+                </div>
+                <div className="page front">
+
+                </div>
+                <div className="page back">
+
+                </div>
             </div>
         </div>
     )
