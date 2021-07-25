bb3e4c9b122f3c3abfa5b2eec0b56a0f638af217
grawish <grawish06@gmail.com>
2021-07-25 14:58:17 +0530

    level traversal backbone added

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,24 +9,32 @@ const Board = () => {
 
     return (
         <div className={'boardContainer'}>
-            <div  className={'board'}>
+            <div className={'board'}>
                 <div className="page top">
-
+                    Hard
                 </div>
                 <div className="page bottom">
-
+                    Extra Easy
                 </div>
                 <div className="page left">
-
+                    Medium
                 </div>
                 <div className="page right">
-
+                    <div className={'cell'}>1</div>
+                    <div className={'cell'}>2</div>
+                    <div className={'cell'}>3</div>
+                    <div className={'cell'}>4</div>
+                    <div className={'cell'}>5</div>
+                    <div className={'cell'}>6</div>
+                    <div className={'cell'}>7</div>
+                    <div className={'cell'}>8</div>
+                    <div className={'cell'}>9</div>
                 </div>
                 <div className="page front">
-
+                    Welcome Page!
                 </div>
                 <div className="page back">
-
+                    Final Page
                 </div>
             </div>
         </div>
