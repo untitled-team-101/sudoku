a9b7e203e16343a33838adefcd19c36575de0200
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-07-26 15:05:29 +0530

    Re marged

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,26 +5,38 @@ import backImg from "./img/back.gif";
 import Cell from "./Cell";
 import {useState} from "react";
 
+import { setCellSelectionFunction } from "./context/SelectedCell";
 import {setBoardStateFunction} from "./context/BoardState";
 
-const Board = ({sudokuArray, editable, solutionArray, difficulty}) => {
+const Board = ({
+                   sudokuArray,
+                   editable,
+                   solutionArray,
+                   difficulty,
+                   setDifficulty,
+               }) => {
     const [board, setBoard] = useState(sudokuArray);
-
+    if (JSON.stringify(board) === JSON.stringify(solutionArray)) {
+        setDifficulty(difficulty + 1);
+    }
+    const [count, setCount] = useState(0);
+    const [selection, setSelection] = useState([-1, -1]);
     setBoardStateFunction(setBoard);
+    setCellSelectionFunction(setSelection);
     let cells = [];
     for (let i = 0; i < board.length; i++) {
         cells.push(
-            <Cell
-                number={board[i]}
-                index={i}
-                setBoard={setBoard}
-                editable={editable[i]}
-                key={i}
-                status={solutionArray[i] === board[i]}
-            />
+          <Cell
+            selection={selection}
+            number={board[i]}
+            index={i}
+            setBoard={setBoard}
+            editable={editable[i]}
+            key={i}
+            status={solutionArray[i] === board[i]}
+          />
         );
     }
-
 
     let backClick = () => {
         document.getElementById('board').classList.add('animate');
@@ -32,20 +44,47 @@ const Board = ({sudokuArray, editable, solutionArray, difficulty}) => {
             window.location = '/#/credits'
         },7000);
     }
+    let arr = [];
+    arr.push(<span className="tooltiptext">Hello</span>);
+    arr.push(<span className="tooltiptext">Neeche to Dekho!!!</span>);
+    arr.push(<span className="tooltiptext">Abe Chutiye!!</span>);
+    arr.push(<span className="tooltiptext">Scroll kar!!!</span>);
+
+    const unsetStyling = {
+        visibility: "unset",
+    };
+
+    const difficultyArray = ["", "xeasy", "easy", "medium", "hard", "done"];
     return (
-        <>
-            <div id={'boardContainer'} className={"boardContainer"}>
-                <div className={"board suggestion"} id={'board'}>
+      <div
+        onWheel={() => {
+            setDifficulty(1);
+            console.log("SCROLL", difficulty);
+        }}
+      >
+          <div
+            id={"boardContainer"}
+            className={`boardContainer ${difficultyArray[difficulty]}`}
+          >
+              <div className={"board suggestion"} id={'board'}>
                     <div className="page top" id={'hard'}>{cells}</div>
                     <div className="page bottom" id={'xeasy'}>{cells}</div>
                     <div className="page left" id={'medium'}>{cells}</div>
                     <div className="page right" id={'easy'}>{cells}</div>
-                    <div className="page front" style={{
+                  <div
+                    onClick={() => {
+                        setCount(count + 1);
+                    }}
+                    className="page front tooltip"
+                    style={{
                         backgroundImage: `url(${frontImg})`,
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        backgroundRepeat: 'no-repeat'
-                    }}/>
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                    }}
+                  >
+                      {arr[count]}
+                  </div>
                     <div className="page back" id={'finalPage'} style={{
                         backgroundImage: `url(${backImg})`,
                         backgroundPosition: 'center',
@@ -79,7 +118,7 @@ const Board = ({sudokuArray, editable, solutionArray, difficulty}) => {
                     })
                 }
             </div>
-        </>
+        </div>
     );
 };
 
