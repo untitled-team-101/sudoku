26974db9782976eadc28683564ba51ab1c1230df
grawish <grawish06@gmail.com>
2021-07-26 08:41:02 +0530

    final animation solved! front page bgimage added!

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,8 @@
 import "./styles/Board.scss";
+import frontImg from "./img/front.gif";
 
 import Cell from "./Cell";
 import {useState} from "react";
-import {Route, Redirect} from 'react-router'
 
 import {setBoardStateFunction} from "./context/BoardState";
 
@@ -23,28 +23,20 @@ const Board = ({sudokuArray, editable, solutionArray, difficulty}) => {
             />
         );
     }
-    if (difficulty === 1) {
-        console.log(difficulty);
-        setTimeout(
-            () => {
-                return (
-                    <Redirect to={'/credits'}/>
-                )
-            }
-            , 8000);
-    }
     return (
         <>
-            <div className={difficulty === 1 ? "flash-bang" : ""} id={"flash-bang"}>
-
-            </div>
-            <div id={'boardContainer'} className={"boardContainer done"}>
+            <div id={'boardContainer'} className={"boardContainer"}>
                 <div className={"board suggestion"}>
                     <div className="page top" id={'hard'}>{cells}</div>
                     <div className="page bottom" id={'xeasy'}>{cells}</div>
                     <div className="page left" id={'medium'}>{cells}</div>
                     <div className="page right" id={'easy'}>{cells}</div>
-                    <div className="page front">Welcome Page!</div>
+                    <div className="page front" style={{
+                        backgroundImage: `url(${frontImg})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }}/>
                     <div className="page back">Final Page</div>
                 </div>
             </div>
